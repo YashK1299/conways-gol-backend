0a45fc2b1ebf31d44b458f9d25301786e694c807
Yash Kothari <kothariy3@gmail.com>
2023-04-30 03:56:14 -0700

    Javadoc for records

--- src/main/java/com/conways_game_of_life/app/rest/model/Cell.java
+++ src/main/java/com/conways_game_of_life/app/rest/model/Cell.java
@@ -1,4 +1,11 @@
 package com.conways_game_of_life.app.rest.model;
 
+/**
+ * A record representing the coordinates of a cell in the
+ * game board representing Conway's Game of Life.
+ *
+ * @param row the row in which the cell is present.
+ * @param col the column in which the cell is present.
+ */
 public record Cell(int row, int col) {
 }
--- src/main/java/com/conways_game_of_life/app/rest/model/GameBoard.java
+++ src/main/java/com/conways_game_of_life/app/rest/model/GameBoard.java
@@ -1,4 +1,10 @@
 package com.conways_game_of_life.app.rest.model;
 
+/**
+ * A record representing the dimensions of the game board for Conway's Game of Life.
+ *
+ * @param rows the number of rows in the board
+ * @param cols the number of columns in the board
+ */
 public record GameBoard(int rows, int cols) {
 }
